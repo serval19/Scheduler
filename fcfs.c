6120daa05d2bdd2501ad44047def5b515e435c78
serval19 <diwatomy04@gmail.com>
2025-08-05 12:33:05 +0530

    fcfs complete

--- fcfs.c
+++ fcfs.c
@@ -21,6 +21,42 @@ void sortbyarrivaltime(struct process p[], int n){
     }
 }
 
+void findtimes(struct process p[], int n){
+    p[0].completion_time=p[0].arrival_time + p[0].burst_time;
+    p[0].turnaround_time = p[0].completion_time - p[0].arrival_time;
+    p[0].waiting_time = p[0].turnaround_time - p[0].burst_time;
+    for(int i=1; i<n; i++){
+        int start_time=(p[i-1].completion_time > p[i].arrival_time) ? p[i-1].completion_time : p[i].arrival_time;
+        p[i].completion_time = start_time + p[i].burst_time;
+        p[i].turnaround_time = p[i].completion_time - p[i].arrival_time;
+        p[i].waiting_time = p[i].turnaround_time - p[i].burst_time;
+
+    }
+}
+
+void printresults(struct process p[], int n){
+    float average_waiting_time =0;
+    float average_turnaround_time =0;
+    printf("P_ID\tArrival_Time\tBurst_Time\tCompletion_Time\tWaiting_Time\tTurnaround_Time\n");
+    printf("-----------------------------------------------------------------------------------\n");
+    for( int i=0;i<n;i++){
+        average_waiting_time+= p[i].waiting_time;
+        average_turnaround_time+= p[i].turnaround_time;
+         printf("P%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n",
+               p[i].pid,
+               p[i].arrival_time,
+               p[i].burst_time,
+               p[i].completion_time,
+               p[i].waiting_time,
+               p[i].turnaround_time);
+
+    }
+    average_waiting_time /=n;
+    average_turnaround_time /=n;
+     printf("\nAverage Waiting Time: %.2f\n", average_waiting_time);
+    printf("Average Turnaround Time: %.2f\n", average_turnaround_time);
+
+}
 int main(){
     int n;
     printf("Enter the number of processes: ");
