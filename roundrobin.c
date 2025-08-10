a79363b4bcd0e004664fffcf817c86b50d0aeb88
serval19 <diwatomy04@gmail.com>
2025-08-11 00:59:52 +0530

    round robin completed

--- roundrobin.c
+++ roundrobin.c
@@ -9,6 +9,42 @@ struct process {
     int completed; // completion flag
 };
 
+void round_robin(struct process p[], int n, int quantum){
+    int time =0;
+    int completed=0;
+    for(int i=0; i<n; i++){
+        p[i].remaining = p[i].burst;
+        p[i].completed =0; //completion flag
+        p[i].waiting=0;
+        p[i].turnaround=0;
+
+    }
+    while(completed!=n){
+        int executed=0; // flag to check if any process was executed in this iteration
+        for(int i=0; i<n; i++){
+            if(!p[i].completed && p[i].arrival <= time){
+                executed =1;
+                if(p[i].remaining > quantum){
+                    time += quantum;
+                    p[i].remaining -=quantum;
+                }
+                else{
+                    time += p[i].remaining;
+                    p[i].remaining =0;
+                    p[i].completed =1;
+                    completed++;
+                    p[i].turnaround = time - p[i].arrival;
+                    p[i].waiting = p[i].turnaround - p[i].burst;
+                }
+            }
+            if(!executed){
+                time++; //increment time if no process was executed
+            }
+        }
+
+    }
+}
+
 int main(){
     int n, quantum;
     printf("Enter the number of processes: ");
@@ -25,6 +61,8 @@ int main(){
 
     round_robin(p, n, quantum);
     float average_waiting_time = 0, average_turnaround_time =0;
+    printf("\nProcess\tArrival\tBurst\tWaiting\tTurnarounds\n");
+
     for (int i = 0; i < n; i++) {
         printf("P%d\t%d\t%d\t%d\t%d\n", 
                p[i].pid,
